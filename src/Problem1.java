54ac4facc92e8ad5efe854c5ec6454105e78fe59
Nikhil Musale <nikhilmusale70@gmail.com>
2021-08-18 11:46:20 +0530

    Added logic for problem2

--- src/Problem1.java
+++ src/Problem1.java
@@ -1,21 +1,34 @@
+import java.util.Scanner;
+
 public class Problem1 {
+    public static int numberOfItems;
+
     public static void main(String[] args) {
-        String item1 = "Rice";
-        int priceForItem1PerKg=20;
-        double weightForItem1InKg=100;
 
-        String item2 = "Pulses";
-        int priceForItem2PerKg=30;
-        double weightForItem2InKg=150;
+        System.out.println("Enter number of items you want to add in inventory");
+        Scanner sc =new Scanner(System.in);
+        numberOfItems = sc.nextInt();
 
-        String item3 = "Wheats";
-        int priceForItem3PerKg=50;
-        double weightForItem3InKg=80;
+        String item[] = new String[numberOfItems];
+        int[] priceForItemPerKg = new int[numberOfItems];
+        int[] weightForItemInKg = new int[numberOfItems];
 
-        Inventory[] inv = new Inventory[3];
-        inv[0]= new Inventory(item1,priceForItem1PerKg,weightForItem1InKg);
-        inv[1]= new Inventory(item2,priceForItem2PerKg,weightForItem2InKg);
-        inv[2]= new Inventory(item3,priceForItem3PerKg,weightForItem3InKg);
+        for (int j=0; j<numberOfItems; j++){
+            sc.nextLine();
+            System.out.println("Enter name for item1 :- ");
+            item[j]=sc.nextLine();
+
+            System.out.println("Enter price per kg for "+item[j]+ " :-");
+            priceForItemPerKg[j]= sc.nextInt();
+
+            System.out.println("Enter weight in kg for "+item[j]+ " :-");
+            weightForItemInKg[j]= sc.nextInt();
+        }
+
+        Inventory[] inv = new Inventory[numberOfItems];
+        for (int j=0 ; j<numberOfItems; j++){
+            inv[j]= new Inventory(item[j], priceForItemPerKg[j], weightForItemInKg[j]);
+        }
 
         for (int i=0;i< inv.length;i++) {
             System.out.println("\n"+inv[i]);
@@ -42,7 +55,7 @@ class Inventory {
     @Override
     public String toString() {
         return item+" Inventory" +" \nPrice of "+ item +" per KG is "+
-                priceOfItemPerKg +"\nWeight of "+item+ " in inventory is "+ weightForItemInKg +
+                priceOfItemPerKg +"\nWeight of "+item+ " in inventory in kg is "+ weightForItemInKg +
                 "\nValue of "+item+ " inventory is "+ (weightForItemInKg*priceOfItemPerKg);
     }
 }
